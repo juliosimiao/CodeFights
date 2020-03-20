06113e4614f3fdecd619fe607838e10c6f7e28d9
julio <julio@localhost.localdomain>
2020-03-20 17:42:21 +0100

    Pair of shoes solution

---
+++ Arcade/The Core/Spring of Integration/pairOfShoes.java
@@ -0,0 +1,31 @@
+boolean pairOfShoes(int[][] shoes) {
+
+    ArrayList<String> mapShoes = new ArrayList<>();
+    
+    for(int i=0;i<shoes.length;i++){
+        String sideSize = "" + shoes[i][0] + shoes[i][1] + "";
+        String otherSide = getOtherSide(shoes[i][0],shoes[i][1]);
+        if(mapShoes.indexOf(otherSide) == -1){
+            mapShoes.add(sideSize);
+        }else {System.out.println("removi");
+            mapShoes.remove(otherSide);
+        }     
+    }  
+    
+    if(mapShoes.size() == 0){
+        return true;
+    }    
+    
+    return false;
+}
+
+String getOtherSide(int side, int size){
+    String otherSide = "";
+    if(side == 0){
+        otherSide = "1" + size; 
+    }else {
+        otherSide = "0" + size;
+    }
+    
+    return otherSide;
+}
